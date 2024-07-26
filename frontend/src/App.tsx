c9a914cfc9fca2f53008c64c76619b4363147c80
rvpm132 <138360091+rvpm132@users.noreply.github.com>
2024-07-26 23:59:10 +0530

    fixed router thing (#7)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
 import MainLayout from "./components/layout/MainLayout";
+import AppRoutes from "./routes/route";
 
 const App = () => {
-  return (<MainLayout />);
+  return (
+    <Router>
+      <CssBaseline />
+      <MainLayout>
+        <AppRoutes />
+      </MainLayout>
+    </Router>
+  );
 };
 
 export default App;
--- frontend/src/routes/route.tsx
+++ frontend/src/routes/route.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from '../pages/HomePage';
-import MoviePage from '../pages/MoviePage';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { HomePage, MoviePage } from "../pages";
 
 const AppRoutes: React.FC = () => {
   return (
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/movies" element={<MoviePage />} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/movies" element={<MoviePage />} />
+    </Routes>
   );
 };
 
